8474ad3c6d0635c5bf8d083e54a084401befbde4
Tyler Buchea <tyler@buchea.com>
2017-08-25 11:47:50 -0700

    Trying something new for dev

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./dist/index.js');
+  module.exports = require('./src/index.js');
 } else {
   console.log('NOW SERVING CARBON NATIVE DEVELOPMENT PACKAGE!');
   module.exports = require('./src/index.js');
