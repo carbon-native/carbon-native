54e250342b89f049669e5b8caac2d836262153f5
Tyler Buchea <tyler@buchea.com>
2017-11-20 12:12:56 -0800

    Fix startup stuff and node version

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 'use strict';
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./dist/index.js');
+  // module.exports = require('./dist/index.js');
+  require('./node_modules/react-native-scripts/build/bin/crna-entry.js');
 } else {
   console.log('NOW SERVING CARBON NATIVE DEVELOPMENT PACKAGE!');
   require('./node_modules/react-native-scripts/build/bin/crna-entry.js');
