8b4fb2df44d721dd792a957b64f4281a9a183958
Tyler Buchea <tyler@buchea.com>
2017-11-20 12:00:33 -0800

    Finish setting up workspaces for when the nohoist option is available

--- setup.sh
+++ setup.sh
@@ -2,3 +2,13 @@
 rm -rf packages/carbon-native/node_modules
 rm -rf packages/kitchen-sink/node_modules
 yarn install
+
+# cd ../kitchen-sink
+# yarn unlink carbon-native
+# cd packages/carbon-native
+# yarn unlink
+
+# cd packages/carbon-native
+# yarn link
+# cd ../kitchen-sink
+# yarn link carbon-native
