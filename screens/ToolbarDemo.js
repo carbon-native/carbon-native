9f5ccf01b203d05d805fd16baf94d91bcb7060e3
Josh Buchea <verctrl@joshbuchea.com>
2017-11-26 14:09:34 -0800

    Show more practical examples in Toolbar demo

--- screens/ToolbarDemo.js
+++ screens/ToolbarDemo.js
@@ -1,27 +1,64 @@
 import React from 'react';
-import { Container, Toolbar, ToolbarTitle, Content } from '../src/index.js';
+import { Text } from 'react-native';
+import {
+  Container,
+  Content,
+  Toolbar,
+  ToolbarTitle,
+  ToolbarLeft,
+  ToolbarRight,
+  P,
+  Button,
+  colors,
+} from '../src/index.js';
+import Icon from '@expo/vector-icons/Feather';
 
 const items = [
   { color: 'primary', titleColor: 'light' },
-  { color: 'secondary', titleColor: 'dark' },
+  { color: 'secondary', titleColor: 'light' },
   { color: 'danger', titleColor: 'light' },
   { color: 'stable', titleColor: 'dark' },
   { color: 'dark', titleColor: 'light' },
 ];
 
-const toolbars = items.map((item, index) => (
-  <Toolbar color={item.color} key={item.color}>
-    <ToolbarTitle color={item.titleColor}>
-      {item.color.charAt(0).toUpperCase() + item.color.slice(1)}
-    </ToolbarTitle>
-  </Toolbar>
-));
-
 export default function ToolbarDemo() {
   return (
     <Container>
-      <Content>{toolbars}</Content>
-      {toolbars}
+      <Toolbar color="stable">
+        <ToolbarLeft />
+
+        <ToolbarTitle color="dark">Secondary Toolbar</ToolbarTitle>
+
+        <ToolbarRight>
+          <Button clear size="xs" onPress={() => alert('Settings')}>
+            <Icon name="settings" size={24} style={{ color: colors.primary }} />
+          </Button>
+        </ToolbarRight>
+      </Toolbar>
+
+      <Content padding>
+        <P>Some content</P>
+      </Content>
+
+      <Toolbar color="stable">
+        <ToolbarLeft>
+          <Button clear size="xs" onPress={() => alert('Undo')}>
+            <Icon
+              name="rotate-ccw"
+              size={24}
+              style={{ color: colors.primary }}
+            />
+          </Button>
+        </ToolbarLeft>
+
+        <ToolbarTitle color="dark">Footer Toolbar</ToolbarTitle>
+
+        <ToolbarRight>
+          <Button clear size="xs" onPress={() => alert('Create new document')}>
+            <Icon name="edit" size={24} style={{ color: colors.primary }} />
+          </Button>
+        </ToolbarRight>
+      </Toolbar>
     </Container>
   );
 }
