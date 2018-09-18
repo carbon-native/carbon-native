11f70a6088df59b0b66b7b52aaeb8cbb24623340
Josh Buchea <verctrl@joshbuchea.com>
2018-09-17 18:50:40 -0700

    Fix Expo App: bump expo, remove react-native-scripts

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 
 if (process.env.NODE_ENV === 'production') {
   // module.exports = require('./dist/index.js');
-  require('./node_modules/react-native-scripts/build/bin/crna-entry.js');
+  require('./node_modules/expo/AppEntry.js');
 } else {
   console.log('NOW SERVING CARBON NATIVE DEVELOPMENT PACKAGE!');
-  require('./node_modules/react-native-scripts/build/bin/crna-entry.js');
+  require('./node_modules/expo/AppEntry.js');
 }
