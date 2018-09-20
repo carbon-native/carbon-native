bf7a70518c9b1f3633f5c755af387aa1ecf827b5
Josh Buchea <verctrl@joshbuchea.com>
2018-09-20 12:28:39 -0700

    Update dependencies and fix Expo App

--- __tests__/Content.test.js
+++ __tests__/Content.test.js
@@ -4,6 +4,8 @@ const { Content: TestComponent } = process.env.TEST_DIST
   ? require('../dist')
   : require('../src');
 
+jest.mock('ScrollView', () => 'ScrollView');
+
 test('renders correctly', () => {
   const rendered = renderer.create(<TestComponent />).toJSON();
   expect(rendered).toBeTruthy();
