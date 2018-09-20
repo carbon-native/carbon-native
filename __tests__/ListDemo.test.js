69fdc0b5c11f5c261c54a7d465f627b8a6081f3c
Tyler Buchea <tyler@buchea.com>
2018-09-19 18:54:50 -0700

    Fix tests by mocking scrollview

--- __tests__/ListDemo.test.js
+++ __tests__/ListDemo.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import { ListDemo as TestComponent } from '../screens';
 
+jest.mock('ScrollView', () => 'ScrollView');
+
 test('renders correctly', () => {
   const rendered = renderer.create(<TestComponent />).toJSON();
   expect(rendered).toBeTruthy();
